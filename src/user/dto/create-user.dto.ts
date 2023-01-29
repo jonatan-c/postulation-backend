8eed4652dc09b60952d4ab4c485d9cb26f5fbace
Jonatan Claros <jona.cc.93@gmail.com>
2023-01-29 12:07:33 -0300

    add regex password register

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,6 +2,7 @@ import {
 	IsEmail,
 	IsOptional,
 	IsString,
+	Matches,
 	MaxLength,
 	MinLength,
 } from 'class-validator';
@@ -18,5 +19,9 @@ export class CreateUserDto {
 	@IsString()
 	@MinLength(8)
 	@MaxLength(20)
+	@Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+		message:
+			'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
+	})
 	password: string;
 }
